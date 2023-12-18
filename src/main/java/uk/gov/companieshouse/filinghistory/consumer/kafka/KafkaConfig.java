d8052fa98cb8bde1f6655ac722b0532a5fb4f14d
sthompsonCH <sthompson@companieshouse.gov.uk>
2023-12-18 16:35:22 +0000

    DSND-2242: Update Retry config
    * Retry config being put in the KafkaConfig file was resulting in
      connection not being established error during loading of spring
      context - specifically this: "[AdminClient clientId=adminclient-1]
      Connection to node -1 (localhost/127.0.0.1:9092) could not be
      established. Broker may not be available."
    * Without adding doNotAutoCreateRetryTopics, with spring context takes
      much longer to load.

--- src/main/java/uk/gov/companieshouse/filinghistory/consumer/kafka/KafkaConfig.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/consumer/kafka/KafkaConfig.java
@@ -11,11 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.retrytopic.DltStrategy;
 import org.springframework.kafka.retrytopic.RetryTopicConfiguration;
@@ -91,6 +87,7 @@ public class KafkaConfig {
             @Value("${consumer.backoff-delay}") int delay) {
         return RetryTopicConfigurationBuilder
                 .newInstance()
+                .doNotAutoCreateRetryTopics() // this is necessary to prevent failing connection during loading of spring app context
                 .maxAttempts(attempts)
                 .fixedBackOff(delay)
                 .useSingleTopicForSameIntervals()
