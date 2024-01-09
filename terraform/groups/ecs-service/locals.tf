1d8a63ed8d28188bbfc66859922587b9d69da1f5
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2024-01-09 11:02:06 +0000

    Update healthcheck path

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -7,7 +7,7 @@ locals {
   container_port              = "8080"
   docker_repo                 = "filing-history-delta-consumer"
   kms_alias                   = "alias/${var.aws_profile}/environment-services-kms"
-  healthcheck_path            = "/healthcheck" #healthcheck path for filing-history-delta-consumer
+  healthcheck_path            = "/filing-history-delta-consumer/healthcheck"
   healthcheck_matcher         = "200"
   vpc_name                    = local.stack_secrets["vpc_name"]
   s3_config_bucket            = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
