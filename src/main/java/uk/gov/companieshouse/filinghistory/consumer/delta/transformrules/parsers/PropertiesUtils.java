f0c5e676119dcabeb973f8b53632ca58313a6c90
johncookch <jcook@companieshouse.gov.uk>
2024-01-24 14:26:07 +0000

    DSND-2351: Fix matching rules with repeated capture groups
    * Matching rules with regexes that contain repeated capture group names were duplicated and edited to that one side of the OR is in a different rule
    * Removed the capture group duplicate renaming from the PropertiesUtils

--- src/main/java/uk/gov/companieshouse/filinghistory/consumer/delta/transformrules/parsers/PropertiesUtils.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/consumer/delta/transformrules/parsers/PropertiesUtils.java
@@ -8,17 +8,16 @@ public class PropertiesUtils {
 
     private static final Pattern GROUP_PATTERN = Pattern.compile("<([^>]*)>");
 
+    private PropertiesUtils() {
+    }
+
     static String convertToCamelCase(String original) {
         Matcher matcher = GROUP_PATTERN.matcher(original);
         String value = original;
         while (matcher.find()) {
             String camelCase = CaseUtils.toCamelCase(matcher.group(1), false, '_');
-            if (value.contains(camelCase)) {
-                value = "%s1".formatted(camelCase);
-            }
             value = value.replace(matcher.group(1), camelCase);
         }
         return value;
     }
-
 }
