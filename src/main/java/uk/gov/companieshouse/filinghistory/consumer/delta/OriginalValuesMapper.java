0691674779582faa7e8c178112951f733c6ef772
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-01-29 12:18:42 +0000

    DSND-2241: Implement original values mapper

--- src/main/java/uk/gov/companieshouse/filinghistory/consumer/delta/OriginalValuesMapper.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/consumer/delta/OriginalValuesMapper.java
@@ -1,5 +1,7 @@
 package uk.gov.companieshouse.filinghistory.consumer.delta;
 
+import static uk.gov.companieshouse.filinghistory.consumer.delta.MappingUtils.getValueFromField;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.filinghistory.InternalDataOriginalValues;
@@ -7,7 +9,9 @@ import uk.gov.companieshouse.api.filinghistory.InternalDataOriginalValues;
 @Component
 public class OriginalValuesMapper {
 
-    public InternalDataOriginalValues map(JsonNode topLevelNode) {
-        return null;
+    public InternalDataOriginalValues map(JsonNode jsonNode) {
+        return new InternalDataOriginalValues()
+                .resignationDate(getValueFromField(jsonNode, "resignation_date"))
+                .officerName(getValueFromField(jsonNode, "officer_name"));
     }
 }
