99ace83072a57ef101e472d1114c963f3afd4954
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-02-02 09:59:59 +0000

    DSND-2240: Parameterise upsert delta service test

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/delta/UpsertDeltaServiceTest.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/delta/UpsertDeltaServiceTest.java
@@ -9,6 +9,8 @@ import java.nio.charset.StandardCharsets;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -34,13 +36,16 @@ class UpsertDeltaServiceTest {
     @Mock
     private InternalFilingHistoryApi apiRequest;
 
-    @Test
-    void shouldSuccessfullyPassDeserialisedAndMappedDeltaToApiClient() throws Exception {
+    @ParameterizedTest
+    @CsvSource({
+            "TM01"
+    })
+    void shouldSuccessfullyPassDeserialisedAndMappedDeltaToApiClient(final String prefix) throws Exception {
         // given
         when(deserialiser.deserialiseFilingHistoryDelta(any())).thenReturn(delta);
         when(mapper.processDelta(any(), anyString())).thenReturn(apiRequest);
 
-        final String jsonString = IOUtils.resourceToString("/TM01_delta.json", StandardCharsets.UTF_8);
+        final String jsonString = IOUtils.resourceToString("/%s_delta.json".formatted(prefix), StandardCharsets.UTF_8);
 
         ChsDelta chsDelta = new ChsDelta(jsonString, 0, "contextId", false);
 
