944035da340ea3000b3fcf61d3e0e275d0dd79ef
cgregoryCH <cgregory@companieshouse.gov.uk>
2024-02-07 14:00:55 +0000

    DSND-2379: Use input stream instead of File

--- src/main/java/uk/gov/companieshouse/filinghistory/consumer/delta/transformrules/TransformerConfig.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/consumer/delta/transformrules/TransformerConfig.java
@@ -3,8 +3,8 @@ package uk.gov.companieshouse.filinghistory.consumer.delta.transformrules;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,8 +21,9 @@ public class TransformerConfig {
     public TransformerService transformRules(@Value("${transform.rules}") String rulesFile,
             TransformerFactory transformerFactory) throws IOException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        File file = new ClassPathResource(rulesFile).getFile();
-        List<RuleProperties> ruleProperties = mapper.readValue(file, new TypeReference<>() {});
+        InputStream file = new ClassPathResource(rulesFile).getInputStream();
+        List<RuleProperties> ruleProperties = mapper.readValue(file, new TypeReference<>() {
+        });
 
         List<Rule> rules = ruleProperties.stream()
                 .map(ruleProperty -> ruleProperty.compile(transformerFactory))
