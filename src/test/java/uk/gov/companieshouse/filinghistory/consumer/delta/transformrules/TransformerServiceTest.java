59f6b68ccd482ea17659dd8ed04171a27dc08ff5
cgregoryCH <cgregory@companieshouse.gov.uk>
2024-02-07 13:42:11 +0000

    DSND-2379: Add transform service test assertions on expected nodes

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/delta/transformrules/TransformerServiceTest.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/delta/transformrules/TransformerServiceTest.java
@@ -1,11 +1,11 @@
 package uk.gov.companieshouse.filinghistory.consumer.delta.transformrules;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +13,7 @@ import uk.gov.companieshouse.filinghistory.consumer.delta.transformrules.functio
 
 class TransformerServiceTest {
 
+    private static final ObjectMapper MAPPER = TransformerTestingUtils.getMapper();
     private static final String TM01_REQUEST_BODY = """
             {
                 "_id" : "mongo_id",
@@ -20,12 +21,16 @@ class TransformerServiceTest {
                 "data" : {
                     "description" : "Appointment Terminated, Director JOE BLOGS",
                     "category" : "officer",
-                    "date" : 20091007140721,
+                    "date" : "20091007140721",
                     "type" : "TM01"
                 },
                 "_entity_id" : "0123456789",
                 "_barcode" : "AAAAAAAA",
-                "_document_id" : "000AAAAAAAA1234"
+                "_document_id" : "000AAAAAAAA1234",
+                "original_values": {
+                    "resignation_date": "06/08/2011",
+                    "officer_name": "Joe Blogs"
+                }
             }""";
 
     private static final String AA_REQUEST_BODY = """
@@ -35,11 +40,8 @@ class TransformerServiceTest {
                 "_document_id" : "000AOKK2A9N0981",
                 "_entity_id" : "151284139",
                 "data" : {
-                    "action_date" : "1998-04-05T00:00:00.000+0000",
-                    "category" : "accounts",
-                    "date" : "1998-10-12T11:59:17.000+0000",
+                    "date" : "19981012115917",
                     "description" : "GROUP ACCOUNTS FOR SMALL CO. MADE UP TO 05/04/98",
-                    "paper_filed" : true,
                     "type" : "AA"
                 }
             }""";
@@ -71,8 +73,7 @@ class TransformerServiceTest {
                 "_id" : "MzA0MTc3MzgzMmFkaXF6a2N4",
                 "_entity_id" : "3041773832",
                 "data" : {
-                    "type" : "DEFAULT",
-                    "description" : "Some unrecognised string"
+                    "description" : "SOME unrecognised string"
                 }
             }
             """;
@@ -88,73 +89,157 @@ class TransformerServiceTest {
     @Test
     void transformTM01() throws JsonProcessingException {
         // given
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode delta = mapper.readTree(TM01_REQUEST_BODY);
+        JsonNode delta = MAPPER.readTree(TM01_REQUEST_BODY);
+
+        ObjectNode expected = MAPPER.createObjectNode();
+
+        expected
+                .put("_id", "mongo_id")
+                .put("company_number", "12345678")
+                .put("_entity_id", "0123456789")
+                .put("_barcode", "AAAAAAAA")
+                .put("_document_id", "000AAAAAAAA1234")
+                .put("original_description", "Appointment terminated, director joe blogs");
+
+        expected
+                .putObject("data")
+                .put("description", "termination-director-company-with-name-termination-date")
+                .put("category", "officers")
+                .put("subcategory", "termination")
+                .put("date", "20091007140721")
+                .put("action_date", "2011-08-06T00:00:00Z")
+                .put("type", "TM01")
+                .putObject("description_values")
+                .put("officer_name", "Joe Blogs")
+                .put("termination_date", "2011-08-06T00:00:00Z");
+
+        expected
+                .putObject("original_values")
+                .put("resignation_date", "06/08/2011")
+                .put("officer_name", "Joe Blogs");
 
         // when
-        JsonNode requestBody = service.transform(delta);
+        JsonNode actual = service.transform(delta);
 
         // then
-        assertNotNull(requestBody);
+        assertEquals(expected, actual);
     }
 
     @Test
     void transformAA() throws JsonProcessingException {
         // given
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode delta = mapper.readTree(AA_REQUEST_BODY);
+        JsonNode delta = MAPPER.readTree(AA_REQUEST_BODY);
+
+        ObjectNode expected = MAPPER.createObjectNode();
+        expected
+                .put("_id", "MTUxMjg0MTM5YWRpcXprY3g")
+                .put("company_number", "14388379")
+                .put("_entity_id", "151284139")
+                .put("_document_id", "000AOKK2A9N0981")
+                .put("original_description", "Group accounts for small co. Made up to 05/04/98");
+
+        expected
+                .putObject("data")
+                .put("description", "accounts-with-accounts-type-small-group")
+                .put("category", "accounts")
+                .put("date", "19981012115917")
+                .put("action_date", "1998-04-05T00:00:00Z")
+                .put("type", "AA")
+                .putObject("description_values")
+                .put("made_up_date", "1998-04-05T00:00:00Z");
 
         // when
-        JsonNode requestBody = service.transform(delta);
+        JsonNode actual = service.transform(delta);
 
         // then
-        assertNotNull(requestBody);
-
-        assertEquals("AA", requestBody.at(TransformerUtils.toJsonPtr("data/type")).textValue());
-        assertEquals("accounts-with-accounts-type-small-group",
-                requestBody.at(TransformerUtils.toJsonPtr("data/description")).textValue());
-        assertEquals("accounts", requestBody.at(TransformerUtils.toJsonPtr("data/category")).textValue());
+        assertEquals(expected, actual);
     }
 
     @Test
     void transformWithDefineAndExec() throws JsonProcessingException {
         // given
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode delta = mapper.readTree(REQUEST_WITH_DEFINE_EXEC);
+        JsonNode delta = MAPPER.readTree(REQUEST_WITH_DEFINE_EXEC);
+
+        ObjectNode expected = MAPPER.createObjectNode();
+        expected
+                .put("_id", "MzA0MTc3MzgzMmFkaXF6a2N4")
+                .put("_entity_id", "3041773832")
+                .put("original_description", "29/07/11 statement of capital gbp 13337");
+
+        ObjectNode capital = MAPPER.createObjectNode();
+        capital
+                .put("figure", "13,337")
+                .put("currency", "GBP");
+
+        expected
+                .putObject("data")
+                .put("description", "capital-allotment-shares")
+                .put("category", "capital")
+                .put("action_date", "2011-07-29T00:00:00Z")
+                .put("type", "SH01")
+                .putObject("description_values")
+                .put("date", "2011-07-29T00:00:00Z")
+                .putArray("capital")
+                .add(capital);
 
         // when
-        JsonNode requestBody = service.transform(delta);
+        JsonNode actual = service.transform(delta);
 
         // then
-        assertNotNull(requestBody);
+        assertEquals(expected, actual);
     }
 
     @Test
     void transformAA01() throws JsonProcessingException {
         // given
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode delta = mapper.readTree(REQUEST_WITH_TWO_LIKE_CAPTURE_GROUPS);
+        JsonNode delta = MAPPER.readTree(REQUEST_WITH_TWO_LIKE_CAPTURE_GROUPS);
+
+        ObjectNode expected = MAPPER.createObjectNode();
+        expected
+                .put("_id", "MzA0MTc3MzgzMmFkaXF6a2N4")
+                .put("_entity_id", "3041773832")
+                .put("original_description", "Prevsho from 29/07/11 to 1/07/12");
+
+        expected
+                .putObject("data")
+                .put("description", "change-account-reference-date-company-previous-shortened")
+                .put("category", "accounts")
+                .put("action_date", "2012-07-01T00:00:00Z")
+                .put("type", "AA01")
+                .putObject("description_values")
+                .put("made_up_date", "2011-07-29T00:00:00Z")
+                .put("new_date", "2012-07-01T00:00:00Z");
 
         // when
-        JsonNode requestBody = service.transform(delta);
+        JsonNode actual = service.transform(delta);
 
         // then
-        assertNotNull(requestBody);
-        System.out.println(requestBody);
+        assertEquals(expected, actual);
     }
 
 
     @Test
     void transformUsingDefaultRule() throws JsonProcessingException {
         // given
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode delta = mapper.readTree(MATCH_DEFAULT_RULE);
+        JsonNode delta = MAPPER.readTree(MATCH_DEFAULT_RULE);
+
+        ObjectNode expected = MAPPER.createObjectNode();
+        expected
+                .put("_id", "MzA0MTc3MzgzMmFkaXF6a2N4")
+                .put("_entity_id", "3041773832")
+                .put("original_description", "Some unrecognised string")
+                .put("matched_default", "1");
+
+        expected
+                .putObject("data")
+                .put("description", "legacy")
+                .putObject("description_values")
+                .put("description", "Some unrecognised string");
 
         // when
-        JsonNode requestBody = service.transform(delta);
+        JsonNode actual = service.transform(delta);
 
         // then
-        assertNotNull(requestBody);
-        System.out.println(requestBody);
+        assertEquals(expected, actual);
     }
 }
