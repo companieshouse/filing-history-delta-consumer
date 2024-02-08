ef391464bf3d8712b6d7ef432c55dc096ccf5a6f
cgregoryCH <cgregory@companieshouse.gov.uk>
2024-02-08 18:18:47 +0000

    DSND-2403: Refactor integration test to use wiremock
    * Wiremock verifies the call to the API is made using a custom
      matcher.

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveIT.java
@@ -1,22 +1,22 @@
 package uk.gov.companieshouse.filinghistory.consumer.kafka;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static com.github.tomakehurst.wiremock.client.WireMock.requestMadeFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
 import static uk.gov.companieshouse.filinghistory.consumer.kafka.KafkaUtils.ERROR_TOPIC;
 import static uk.gov.companieshouse.filinghistory.consumer.kafka.KafkaUtils.INVALID_TOPIC;
 import static uk.gov.companieshouse.filinghistory.consumer.kafka.KafkaUtils.MAIN_TOPIC;
 import static uk.gov.companieshouse.filinghistory.consumer.kafka.KafkaUtils.RETRY_TOPIC;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 import org.apache.avro.io.DatumWriter;
 import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
@@ -29,27 +29,20 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import uk.gov.companieshouse.api.InternalApiClient;
-import uk.gov.companieshouse.api.filinghistory.InternalFilingHistoryApi;
-import uk.gov.companieshouse.api.handler.delta.PrivateDeltaResourceHandler;
-import uk.gov.companieshouse.api.handler.delta.filinghistory.request.PrivateFilingHistoryPut;
-import uk.gov.companieshouse.api.http.HttpClient;
 import uk.gov.companieshouse.delta.ChsDelta;
-import uk.gov.companieshouse.filinghistory.consumer.delta.ResponseHandler;
 
 @SpringBootTest
 class ConsumerPositiveIT extends AbstractKafkaIT {
 
     private static final String TRANSACTION_ID = "MzA0Mzk3MjY3NXNhbHQ";
     private static final String COMPANY_NUMBER = "12345678";
-    private static final String PUT_REQUEST_URI = "/filing-history-data-api/company/%s/filing-history/%s/internal".formatted(COMPANY_NUMBER, TRANSACTION_ID);
+    private static final String PUT_REQUEST_URI = "/filing-history-data-api/company/%s/filing-history/%s/internal".formatted(
+            COMPANY_NUMBER, TRANSACTION_ID);
 
     @Autowired
     private KafkaConsumer<String, byte[]> testConsumer;
@@ -60,18 +53,7 @@ class ConsumerPositiveIT extends AbstractKafkaIT {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
-    private Supplier<InternalApiClient> internalApiClientSupplier;
-    @Mock
-    private InternalApiClient internalApiClient;
-    @Mock
-    private HttpClient apiClient;
-    @Mock
-    private PrivateDeltaResourceHandler privateDeltaResourceHandler;
-    @Mock
-    private PrivateFilingHistoryPut privateFilingHistoryPut;
-    @Mock
-    private ResponseHandler responseHandler;
+    private WireMockServer server;
 
     @DynamicPropertySource
     static void props(DynamicPropertyRegistry registry) {
@@ -80,6 +62,8 @@ class ConsumerPositiveIT extends AbstractKafkaIT {
 
     @BeforeEach
     public void setup() {
+        server = new WireMockServer();
+        server.start();
         testConsumer.poll(Duration.ofSeconds(1));
     }
 
@@ -88,37 +72,34 @@ class ConsumerPositiveIT extends AbstractKafkaIT {
             "TM01"
     })
     void testConsumeFromStreamFilingHistoryDeltaTopic(final String prefix) throws Exception {
-        //given
+        // given
         final String delta = IOUtils.resourceToString("/%s_delta.json".formatted(prefix), StandardCharsets.UTF_8);
-        final String requestBody = IOUtils.resourceToString("/%s_request_body.json".formatted(prefix), StandardCharsets.UTF_8);
-        final InternalFilingHistoryApi expectedRequestBody = objectMapper.readValue(requestBody, InternalFilingHistoryApi.class);
+        final String requestBody = IOUtils.resourceToString("/%s_request_body.json".formatted(prefix),
+                StandardCharsets.UTF_8);
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         Encoder encoder = EncoderFactory.get().directBinaryEncoder(outputStream, null);
         DatumWriter<ChsDelta> writer = new ReflectDatumWriter<>(ChsDelta.class);
         writer.write(new ChsDelta(delta, 0, "context_id", false), encoder);
 
-        when(internalApiClientSupplier.get()).thenReturn(internalApiClient);
-        when(internalApiClient.getHttpClient()).thenReturn(apiClient);
-        when(internalApiClient.privateDeltaResourceHandler()).thenReturn(privateDeltaResourceHandler);
-        when(privateDeltaResourceHandler.putFilingHistory(any(), any())).thenReturn(privateFilingHistoryPut);
+        server.stubFor(put(urlEqualTo(PUT_REQUEST_URI))
+                .willReturn(aResponse()
+                        .withStatus(200)));
 
-        //when
+        // when
         testProducer.send(new ProducerRecord<>(MAIN_TOPIC, 0, System.currentTimeMillis(),
                 "key", outputStream.toByteArray()));
         if (!latchAspect.getLatch().await(5L, TimeUnit.SECONDS)) {
             fail("Timed out waiting for latch");
         }
 
-        //then
+        // then
         ConsumerRecords<?, ?> consumerRecords = KafkaTestUtils.getRecords(testConsumer, Duration.ofMillis(10000L), 1);
         assertThat(KafkaUtils.noOfRecordsForTopic(consumerRecords, MAIN_TOPIC)).isOne();
         assertThat(KafkaUtils.noOfRecordsForTopic(consumerRecords, RETRY_TOPIC)).isZero();
         assertThat(KafkaUtils.noOfRecordsForTopic(consumerRecords, ERROR_TOPIC)).isZero();
         assertThat(KafkaUtils.noOfRecordsForTopic(consumerRecords, INVALID_TOPIC)).isZero();
 
-        verify(privateDeltaResourceHandler).putFilingHistory(PUT_REQUEST_URI, expectedRequestBody);
-        verify(privateFilingHistoryPut).execute();
-        verifyNoInteractions(responseHandler);
+        server.verify(requestMadeFor(new PutRequestMatcher(PUT_REQUEST_URI, requestBody)));
     }
 }
