4ffb81f14dfa6d212d68d20d22238a38d94b6bfb
cgregoryCH <cgregory@companieshouse.gov.uk>
2024-02-13 10:58:27 +0000

    DSND-2413: Add application test
    * Refactor wiremock test to reset before each

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/FilingHistoryApplicationIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/FilingHistoryApplicationIT.java
@@ -1,6 +1,6 @@
 package uk.gov.companieshouse.filinghistory.consumer;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -8,25 +8,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static uk.gov.companieshouse.logging.util.LogContextProperties.REQUEST_ID;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 @AutoConfigureMockMvc
-@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FilingHistoryApplicationIT {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext context;
-
     @Test
-    void shouldLoadApplicationContext() {
-        assertNotNull(context);
+    void shouldStartApplication() {
+        Executable executable = () -> Application.main(new String[0]);
+        assertDoesNotThrow(executable);
     }
 
     @Test
--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveIT.java
@@ -26,6 +26,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -53,17 +54,21 @@ class ConsumerPositiveIT extends AbstractKafkaIT {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private WireMockServer server;
+    private static final WireMockServer server = new WireMockServer(8888);
 
     @DynamicPropertySource
     static void props(DynamicPropertyRegistry registry) {
         registry.add("steps", () -> 1);
     }
 
+    @BeforeAll
+    static void beforeAll() {
+        server.start();
+    }
+
     @BeforeEach
     public void setup() {
-        server = new WireMockServer();
-        server.start();
+        server.resetAll();
         testConsumer.poll(Duration.ofSeconds(1));
     }
 
