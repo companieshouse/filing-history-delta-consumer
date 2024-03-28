635737e278276832beeb476d875c27cad75c48f3
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-03-28 15:32:50 +0000

    DSND-2138: Add test coverage to TranasctionKindService
    * Added test for handling annotation and associated-filing form
      types.
    * Test coverage for this class is still sayin 80% but it is unclear
      what is missing.

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/service/TransactionKindServiceTest.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/service/TransactionKindServiceTest.java
@@ -1,33 +1,64 @@
 package uk.gov.companieshouse.filinghistory.consumer.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.api.filinghistory.InternalData.TransactionKindEnum;
 
+@ExtendWith(MockitoExtension.class)
 class TransactionKindServiceTest {
 
     private static final String SALT = "salt";
 
+    @InjectMocks
     private TransactionKindService kindService;
+
+    @Mock
+    private FormTypeService formTypeService;
 
     @BeforeEach
     void setUp() {
-        kindService = new TransactionKindService(new FormTypeService(Collections.emptyList()), SALT);
+        kindService = new TransactionKindService(formTypeService, "salt");
     }
 
     @Test
-    void shouldSuccessfullyEncodeIdByTransactionKind() {
+    void shouldSuccessfullyEncodeIdByEntityIdWhenFormTypeServiceReturnsTopLevel() {
         // given
         TransactionKindCriteria criteria = new TransactionKindCriteria("entityId", "", "TM01", "", "");
 
+        when(formTypeService.getFormType(any())).thenReturn("TOP-LEVEL");
+
         TransactionKindResult expected = new TransactionKindResult("ZW50aXR5SWRzYWx0", TransactionKindEnum.TOP_LEVEL);
+
+        // when
+        TransactionKindResult actual = kindService.encodeIdByTransactionKind(criteria);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "ANNOTATION , annotation",
+            "ASSOCIATED-FILING , associated-filing"
+    })
+    void shouldSuccessfullyEncodeIdByParentEntityIdWhenFormTypeServiceReturnsNonTopLevel(final String returnedFormType,
+                                                                                         final String expectedEnumValue) {
+        // given
+        TransactionKindCriteria criteria = new TransactionKindCriteria("entityId", "parentEntityId", "ANY", "ANY", "");
+
+        when(formTypeService.getFormType(any())).thenReturn(returnedFormType);
+
+        TransactionKindResult expected = new TransactionKindResult("cGFyZW50RW50aXR5SWRzYWx0", TransactionKindEnum.fromValue(expectedEnumValue));
 
         // when
         TransactionKindResult actual = kindService.encodeIdByTransactionKind(criteria);
@@ -41,10 +72,12 @@ class TransactionKindServiceTest {
             "null",
             "''"
     },
-    nullValues = {"null"})
+            nullValues = {"null"})
     void shouldReturnUnchangedIdIfNullOrEmpty(final String entityId) {
         // given
         TransactionKindCriteria criteria = new TransactionKindCriteria(entityId, "", "TM01", "", "");
+
+        when(formTypeService.getFormType(any())).thenReturn("TOP-LEVEL");
 
         TransactionKindResult expected = new TransactionKindResult(entityId, TransactionKindEnum.TOP_LEVEL);
 
