1bb5f2f940048fd6fee960eca43b800f7ad16a83
cgregoryCH <cgregory@companieshouse.gov.uk>
2024-04-04 11:28:31 +0100

    DSND-2138: Refactor annotations WIP

--- src/main/java/uk/gov/companieshouse/filinghistory/consumer/mapper/ChildRequestMapper.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.companieshouse.filinghistory.consumer.mapper;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import uk.gov.companieshouse.api.filinghistory.InternalFilingHistoryApi;
-
-public interface ChildRequestMapper {
-
-    InternalFilingHistoryApi map(JsonNode dataNode);
-}
