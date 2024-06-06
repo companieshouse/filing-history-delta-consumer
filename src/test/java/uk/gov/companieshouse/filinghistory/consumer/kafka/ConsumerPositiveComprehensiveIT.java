009de5a9494afd06b12c500ec2316b4c7e31026f
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-06-06 16:11:07 +0100

    DSND-2688: WIP - Start writing tests
    * Tests need to cover examples where description_values does AND
      does not exist on the delta.
    * Transform rules need to get psc_name and cessation_date from
      capture groups rather than directly from fields.

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveComprehensiveIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveComprehensiveIT.java
@@ -69,64 +69,68 @@ class ConsumerPositiveComprehensiveIT extends AbstractKafkaIT {
     //  failing due to missing psc_name in description value object on delta
     @ParameterizedTest
     @CsvSource({
-            "change-of-name/CONDIR",
+//            "change-of-name/CONDIR",
+//
+//            "resolution/RES01", "resolution/RES01_no_barcode", "resolution/RES15_top_level", "resolution/RES15_child",
+//
+//            "annotation/annotation", "annotation/top_level_annotation",
+//
+//            "annual_return/363s",
+//
+//            "officers/EW01RSS", "officers/TM01", "officers/RP04LLPSC05", "officers/RP04LLPSC04", "officers/RP04PSC09",
+//            "officers/RP04PSC07", "officers/RP04PSC03", "officers/RP04LLPSC02", "officers/RP04LLPSC03",
+//            "officers/RP04PSC01", "officers/RP04PSC04", "officers/RP04PSC02", "officers/RP04LLPSC01",
+//            "officers/RP04PSC05", "officers/RP04LLPSC07",
+//
+//            "capital/SH03", "capital/SH07", "capital/SH01", "capital/SH02_rule_2", "capital/SH04_rule_4",
+//            "capital/SH05", "capital/EW05RSS",
+//
+//            "accounts/AA_rule_17", "accounts/AA_rule_9", "accounts/AA_rule_10", "accounts/AA_rule_8",
+//            "accounts/AA_rule_6", "accounts/AA_rule_12", "accounts/AA_rule_14", "accounts/AA_rule_20",
+//            "accounts/AA_rule_23", "accounts/AA_rule_25", "accounts/AA_rule_26", "accounts/AAMD_rule_12",
+//            "accounts/AAMD_rule_9", "accounts/AAMD_rule_1", "accounts/AAMD_rule_7",
+//
+//            "address/287",
+//
+//            "incorporation/child_transaction/model_articles", "incorporation/child_transaction/newinc",
+//
+//            "insolvency/3.10", "insolvency/4.13", "insolvency/4.20_rule_2", "insolvency/4.31", "insolvency/4.33",
+//            "insolvency/4.35", "insolvency/4.38", "insolvency/4.40", "insolvency/4.43", "insolvency/WU15(Scot)",
+//            "insolvency/WU16(Scot)", "insolvency/WU17(Scot)", "insolvency/WU18(Scot)", "insolvency/4.44",
+//            "insolvency/4.46", "insolvency/4.48", "insolvency/4.51", "incorporation/OE01", "insolvency/4.68_rule_3",
+//            "insolvency/4.20_rule_1", "insolvency/4.68_rule_1", "insolvency/4.69", "insolvency/4.70", "insolvency/4.71",
+//            "insolvency/4.72", "insolvency/4.17(Scot)", "insolvency/4.9(Scot)", "insolvency/C04.2(Scot)",
+//            "insolvency/WU01", "insolvency/WU01(Scot)", "insolvency/C0LIQ", "insolvency/COCOMP_rule_2",
+//            "insolvency/WU07", "insolvency/WU08", "insolvency/WU09", "insolvency/WU11", "insolvency/WU12",
+//            "insolvency/WU14", "insolvency/AM11", "insolvency/2.24B_rule_1", "insolvency/2.24B_rule_2",
+//            "insolvency/2.26B", "insolvency/2.27B", "insolvency/2.28B", "insolvency/2.30B", "insolvency/AM20(Scot)",
+//            "insolvency/2.31B", "insolvency/2.32B", "insolvency/2.33B", "insolvency/2.34B", "insolvency/2.35B_rule_1",
+//            "insolvency/2.35B_rule_2", "insolvency/2.36B", "insolvency/2.38B", "insolvency/2.39B", "insolvency/AM16",
+//            "insolvency/AM17", "insolvency/AM18", "insolvency/2.40B", "insolvency/2.31B(Scot)", "insolvency/2.12B",
+//            "insolvency/AM11(Scot)", "insolvency/AM12", "insolvency/2.16B_rule_1", "insolvency/AM02(Scot)_rule_1",
+//            "insolvency/2.16B_rule_2", "insolvency/2.17B", "insolvency/AM04(Scot)", "insolvency/AM05(Scot)",
+//            "insolvency/2.22B", "insolvency/2.23B", "insolvency/AM08(Scot)", "insolvency/AM07(Scot)",
+//            "insolvency/AM01(Scot)", "insolvency/2.15B_rule_1", "insolvency/2.15B_rule_2", "insolvency/2.16B(Scot)",
+//            "insolvency/AM03(Scot)", "insolvency/2.17B(Scot)", "insolvency/AM09(Scot)", "insolvency/2.16BZ(Scot)",
+//            "insolvency/2.20B(Scot)_rule_2", "insolvency/2.19B(Scot)", "insolvency/2.21B(Scot)",
+//            "insolvency/2.22B(Scot)", "insolvency/2.23B(Scot)", "insolvency/AM21(Scot)", "insolvency/2.24B(Scot)",
+//            "insolvency/AM25(Scot)", "insolvency/2.25B(Scot)", "insolvency/AM22(Scot)",
+//            "insolvency/2.26B(Scot)",
+//            "insolvency/1(Scot)", "insolvency/1.3(Scot)_rule_1", "insolvency/1.3(Scot)_rule_2",
+//            "insolvency/1.4(Scot)", "insolvency/2.2(Scot)", "insolvency/2.12(Scot)", "insolvency/2.26B(Scot)",
+//            "insolvency/2.27B(Scot)", "insolvency/2.29B(Scot)", "insolvency/2.30B(Scot)", "insolvency/12.1",
+//            "insolvency/AM15(Scot)", "insolvency/AM23(Scot)", "insolvency/NCOP", "insolvency/RM01(Scot)",
+//            "mortgage/MR01_rule_4", "mortgage/MR01_rule_5", "mortgage/MR02_rule_3", "mortgage/MR02_rule_4",
+//            "mortgage/MR03_rule_4",
+//            "mortgage/MR04_rule_1", "mortgage/MR04_rule_2", "mortgage/MR04_rule_3", "mortgage/MR05_rule_1",
+//            "mortgage/MR05_rule_2", "mortgage/MR05_rule_3", "mortgage/MR05_rule_4", "mortgage/MR05_rule_5",
+//            "mortgage/MR05_rule_6", "mortgage/MR05_rule_7", "mortgage/MR06", "mortgage/MR07", "mortgage/MR08",
+//            "mortgage/MR09", "mortgage/MR10", "mortgage/LLMR03", "mortgage/466(Scot)", "mortgage/LLP466(Scot)",
+//            "mortgage/402R(NI)",
 
-            "resolution/RES01", "resolution/RES01_no_barcode", "resolution/RES15_top_level", "resolution/RES15_child",
+            "mortgage/LLPSC07", "mortgage/LLPSC07_no_description_values"
 
-            "annotation/annotation", "annotation/top_level_annotation",
-
-            "annual_return/363s",
-
-            "officers/EW01RSS", "officers/TM01", "officers/RP04LLPSC05", "officers/RP04LLPSC04", "officers/RP04PSC09",
-            "officers/RP04PSC07", "officers/RP04PSC03", "officers/RP04LLPSC02", "officers/RP04LLPSC03",
-            "officers/RP04PSC01", "officers/RP04PSC04", "officers/RP04PSC02", "officers/RP04LLPSC01",
-            "officers/RP04PSC05", "officers/RP04LLPSC07",
-
-            "capital/SH03", "capital/SH07", "capital/SH01", "capital/SH02_rule_2", "capital/SH04_rule_4",
-            "capital/SH05", "capital/EW05RSS",
-
-            "accounts/AA_rule_17", "accounts/AA_rule_9", "accounts/AA_rule_10", "accounts/AA_rule_8",
-            "accounts/AA_rule_6", "accounts/AA_rule_12", "accounts/AA_rule_14", "accounts/AA_rule_20",
-            "accounts/AA_rule_23", "accounts/AA_rule_25", "accounts/AA_rule_26", "accounts/AAMD_rule_12",
-            "accounts/AAMD_rule_9", "accounts/AAMD_rule_1", "accounts/AAMD_rule_7",
-
-            "address/287",
-
-            "incorporation/child_transaction/model_articles", "incorporation/child_transaction/newinc",
-
-            "insolvency/3.10", "insolvency/4.13", "insolvency/4.20_rule_2", "insolvency/4.31", "insolvency/4.33",
-            "insolvency/4.35", "insolvency/4.38", "insolvency/4.40", "insolvency/4.43", "insolvency/WU15(Scot)",
-            "insolvency/WU16(Scot)", "insolvency/WU17(Scot)", "insolvency/WU18(Scot)", "insolvency/4.44",
-            "insolvency/4.46", "insolvency/4.48", "insolvency/4.51", "incorporation/OE01", "insolvency/4.68_rule_3",
-            "insolvency/4.20_rule_1", "insolvency/4.68_rule_1", "insolvency/4.69", "insolvency/4.70", "insolvency/4.71",
-            "insolvency/4.72", "insolvency/4.17(Scot)", "insolvency/4.9(Scot)", "insolvency/C04.2(Scot)",
-            "insolvency/WU01", "insolvency/WU01(Scot)", "insolvency/C0LIQ", "insolvency/COCOMP_rule_2",
-            "insolvency/WU07", "insolvency/WU08", "insolvency/WU09", "insolvency/WU11", "insolvency/WU12",
-            "insolvency/WU14", "insolvency/AM11", "insolvency/2.24B_rule_1", "insolvency/2.24B_rule_2",
-            "insolvency/2.26B", "insolvency/2.27B", "insolvency/2.28B", "insolvency/2.30B", "insolvency/AM20(Scot)",
-            "insolvency/2.31B", "insolvency/2.32B", "insolvency/2.33B", "insolvency/2.34B", "insolvency/2.35B_rule_1",
-            "insolvency/2.35B_rule_2", "insolvency/2.36B", "insolvency/2.38B", "insolvency/2.39B", "insolvency/AM16",
-            "insolvency/AM17", "insolvency/AM18", "insolvency/2.40B", "insolvency/2.31B(Scot)", "insolvency/2.12B",
-            "insolvency/AM11(Scot)", "insolvency/AM12", "insolvency/2.16B_rule_1", "insolvency/AM02(Scot)_rule_1",
-            "insolvency/2.16B_rule_2", "insolvency/2.17B", "insolvency/AM04(Scot)", "insolvency/AM05(Scot)",
-            "insolvency/2.22B", "insolvency/2.23B", "insolvency/AM08(Scot)", "insolvency/AM07(Scot)",
-            "insolvency/AM01(Scot)", "insolvency/2.15B_rule_1", "insolvency/2.15B_rule_2", "insolvency/2.16B(Scot)",
-            "insolvency/AM03(Scot)", "insolvency/2.17B(Scot)", "insolvency/AM09(Scot)", "insolvency/2.16BZ(Scot)",
-            "insolvency/2.20B(Scot)_rule_2", "insolvency/2.19B(Scot)", "insolvency/2.21B(Scot)",
-            "insolvency/2.22B(Scot)", "insolvency/2.23B(Scot)", "insolvency/AM21(Scot)", "insolvency/2.24B(Scot)",
-            "insolvency/AM25(Scot)", "insolvency/2.25B(Scot)", "insolvency/AM22(Scot)",
-            "insolvency/2.26B(Scot)",
-            "insolvency/1(Scot)", "insolvency/1.3(Scot)_rule_1", "insolvency/1.3(Scot)_rule_2",
-            "insolvency/1.4(Scot)", "insolvency/2.2(Scot)", "insolvency/2.12(Scot)", "insolvency/2.26B(Scot)",
-            "insolvency/2.27B(Scot)", "insolvency/2.29B(Scot)", "insolvency/2.30B(Scot)", "insolvency/12.1",
-            "insolvency/AM15(Scot)", "insolvency/AM23(Scot)", "insolvency/NCOP", "insolvency/RM01(Scot)",
-            "mortgage/MR01_rule_4", "mortgage/MR01_rule_5", "mortgage/MR02_rule_3", "mortgage/MR02_rule_4",
-            "mortgage/MR03_rule_4",
-            "mortgage/MR04_rule_1", "mortgage/MR04_rule_2", "mortgage/MR04_rule_3", "mortgage/MR05_rule_1",
-            "mortgage/MR05_rule_2", "mortgage/MR05_rule_3", "mortgage/MR05_rule_4", "mortgage/MR05_rule_5",
-            "mortgage/MR05_rule_6", "mortgage/MR05_rule_7", "mortgage/MR06", "mortgage/MR07", "mortgage/MR08",
-            "mortgage/MR09", "mortgage/MR10", "mortgage/LLMR03", "mortgage/466(Scot)", "mortgage/LLP466(Scot)",
-            "mortgage/402R(NI)",
+//            "mortgage/PSC07"
 
             // New failing tests below here
 //            "officers/RP04SLPPSC04"
