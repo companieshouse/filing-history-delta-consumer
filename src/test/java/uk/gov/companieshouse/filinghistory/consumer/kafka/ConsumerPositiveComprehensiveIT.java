a693e5af71cc2e155d5786d9faa85bd048f0b0f8
John Cook <122292773+johncook-ch@users.noreply.github.com>
2024-07-26 12:32:36 +0100

    DSND-2822: Fixed transform rules for RES deltas (#106)
    * DSND-2822: Fixed transform rules for RES deltas
    
    - Added a new transform rule for RES deltas
    - Updated `ConsumerPositiveComprehensiveIT` with test data for a RES delta
    
    * DSND-2822: Excluded a transitive dependency with a CVE
    
    - Excluded com.nimbusds:nimbus-jose-jwt that was a transitive dependency of
      `uk.gov.companieshouse:sdk-manager-java`
    
    * DSND-2822: Updated parent POM version to fix security check build failures
    
    * DSND-2822: Reverted Kafka polling timeout
    
    ---------
    
    Co-authored-by: johncookch <jcook@companieshouse.gov.uk>

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveComprehensiveIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveComprehensiveIT.java
@@ -74,7 +74,7 @@ class ConsumerPositiveComprehensiveIT extends AbstractKafkaIT {
             "change-of-name/CERT22_child", "change-of-name/RR08",
 
             "resolution/RES01", "resolution/RES01_no_barcode", "resolution/RES15_top_level", "resolution/RES15_child",
-            "resolution/LRESSP",
+            "resolution/LRESSP", "resolution/RES",
 
             "annotation/annotation", "annotation/top_level_annotation",
 
