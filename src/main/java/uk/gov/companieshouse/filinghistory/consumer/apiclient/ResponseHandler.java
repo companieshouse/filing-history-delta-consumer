7414e445b8fa5b3296ea3f2b96c1148333836109
kinpang-CH <kpang@companieshouse.gov.uk>
2024-08-27 09:28:05 +0100

    Update error logging message

--- src/main/java/uk/gov/companieshouse/filinghistory/consumer/apiclient/ResponseHandler.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/consumer/apiclient/ResponseHandler.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.filinghistory.consumer.apiclient;
 
 import static uk.gov.companieshouse.filinghistory.consumer.Application.NAMESPACE;
 
+import java.util.Arrays;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
@@ -16,6 +17,7 @@ import uk.gov.companieshouse.logging.LoggerFactory;
 public class ResponseHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
+    private static final String API_ERROR_LOG_MESSAGE = "Call to API failed, status code: %d. %s";
     private static final String API_ERROR_RESPONSE_MESSAGE = "HTTP response code %d when calling filing history API";
     private static final String URI_VALIDATION_EXCEPTION_MESSAGE = "Failed call to filing history API due to invalid URI";
 
@@ -24,10 +26,12 @@ public class ResponseHandler {
         final HttpStatus httpStatus = HttpStatus.valueOf(ex.getStatusCode());
 
         if (HttpStatus.CONFLICT.equals(httpStatus) || HttpStatus.BAD_REQUEST.equals(httpStatus)) {
-            LOGGER.error(API_ERROR_RESPONSE_MESSAGE.formatted(statusCode), DataMapHolder.getLogMap());
+            LOGGER.error(API_ERROR_LOG_MESSAGE.formatted(statusCode, Arrays.toString(ex.getStackTrace())),
+                    DataMapHolder.getLogMap());
             throw new NonRetryableException(API_ERROR_RESPONSE_MESSAGE.formatted(statusCode), ex);
         }  else {
-            LOGGER.info(API_ERROR_RESPONSE_MESSAGE.formatted(statusCode), DataMapHolder.getLogMap());
+            LOGGER.info(API_ERROR_LOG_MESSAGE.formatted(statusCode, Arrays.toString(ex.getStackTrace())),
+                    DataMapHolder.getLogMap());
             throw new RetryableException(API_ERROR_RESPONSE_MESSAGE.formatted(statusCode), ex);
         }
     }
