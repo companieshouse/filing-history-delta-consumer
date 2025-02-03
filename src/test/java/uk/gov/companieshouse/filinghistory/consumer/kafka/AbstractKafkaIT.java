1c273861e0723a46d068d151165720f105112f05
sthompsonCH <sthompson@companieshouse.gov.uk>
2025-02-03 15:13:47 +0000

    DSND-3241: Upgrade pom deps

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/AbstractKafkaIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/AbstractKafkaIT.java
@@ -3,9 +3,9 @@ package uk.gov.companieshouse.filinghistory.consumer.kafka;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
@@ -13,7 +13,7 @@ import org.testcontainers.utility.DockerImageName;
 public abstract class AbstractKafkaIT {
 
     @Container
-    protected static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse(
+    protected static final ConfluentKafkaContainer kafka = new ConfluentKafkaContainer(DockerImageName.parse(
             "confluentinc/cp-kafka:latest"));
 
     @DynamicPropertySource
