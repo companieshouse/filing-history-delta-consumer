bfa3d10624e81e353fb1fdc852d9184302c76785
wdhda-ch <127737882+wdhda-ch@users.noreply.github.com>
2025-07-22 09:27:04 +0100

    Add more tests cases for the additional parent form type parameter

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/mapper/posttransform/PaperFieldMapperTest.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/mapper/posttransform/PaperFieldMapperTest.java
@@ -9,28 +9,36 @@ class PaperFieldMapperTest {
 
     private final PaperFiledMapper mapper = new PaperFiledMapper();
 
-    @ParameterizedTest(name = "Test isPaperFiled should be {2} with barcode: {0}, formType: {1}")
+    @ParameterizedTest(name = "Test isPaperFiled should be {3} with barcode: {0}, formType: {1}, parentFormType: {2}")
     @CsvSource(value = {
-            "XAITVXAX , null , false",
-            "TAITVXAX , null , true",
-            "null , null , true",
-            "'' , null , true",
-            "XAITVXAX , ANNOTATION , false",
-            "TAITVXAX , ANNOTATION , false",
-            "null , ANNOTATION , false",
-            "'' , ANNOTATION , false",
-            "XAITVXAX , '' , false",
-            "TAITVXAX , '' , true",
-            "null , '' , true",
-            "'' , '' , true",
+            "XAITVXAX , null , AA , false",
+            "TAITVXAX , null , AA , true",
+            "null , null , AA , true",
+            "'' , null , AA , true",
+            "XAITVXAX , ANNOTATION , AA , false",
+            "XAITVXAX , ANNOTATION , OTHER , false",
+            "XAITVXAX , ANNOTATION , AAMD , false",
+            "TAITVXAX , ANNOTATION , AA , false",
+            "null , ANNOTATION , AA , false",
+            "null , ANNOTATION , OTHER , true",
+            "'' , ANNOTATION , AA , false",
+            "'' , ANNOTATION , OTHER , true",
+            "XAITVXAX , '' , AA , false",
+            "TAITVXAX , '' , AA , true",
+            "null , '' , AA , true",
+            "'' , '' , AA , true",
+            "XAITVXAX , ANNOTATION , null , false",
+            "TAITVXAX , ANNOTATION , null , true",
+            "null , ANNOTATION , null , true",
+            "'' , ANNOTATION , null , true",
             
     },
             nullValues = {"null"})
-    void testPaperFieldReturnsBooleanCorrectly(final String barcode, final String formType, final boolean expected) {
+    void testPaperFieldReturnsBooleanCorrectly(final String barcode, final String formType, final String parentFormType, final boolean expected) {
         // given
 
         // when
-        final boolean actual = mapper.isPaperFiled(barcode, formType);
+        final boolean actual = mapper.isPaperFiled(barcode, formType, parentFormType);
 
         // then
         assertEquals(expected, actual);
