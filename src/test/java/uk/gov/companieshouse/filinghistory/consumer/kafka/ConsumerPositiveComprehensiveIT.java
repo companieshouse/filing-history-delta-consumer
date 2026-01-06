c6e8e81e9d864657ea1bcc73a17c17a0d020695c
mwestacott <mwestacott2@companieshouse.gov.uk>
2026-01-06 11:19:12 +0000

    LP-1348 Map the new LPTS01 filing history form data
    * Rules YAML file modified to have a new mapping for the LPTS01
    * New mapping defines a new api-enumerations key
    * Integration test updated and new test resources added

--- src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveComprehensiveIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/consumer/kafka/ConsumerPositiveComprehensiveIT.java
@@ -114,6 +114,7 @@ class ConsumerPositiveComprehensiveIT extends AbstractKafkaIT {
             "incorporation/CERT12B", "incorporation/CICINC",
             "incorporation/LP7D",
             "incorporation/LP5D",
+            "incorporation/LPTS01",
 
             "insolvency/3.10", "insolvency/4.13", "insolvency/4.20_rule_2", "insolvency/4.31", "insolvency/4.33",
             "insolvency/4.35", "insolvency/4.38", "insolvency/4.40", "insolvency/4.43", "insolvency/WU15(Scot)",
